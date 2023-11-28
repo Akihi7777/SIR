3264a1ac3f4113cc9ce24628450d63de4bba42af
akihi7777 <1923418219@qq.com>
2023-11-28 22:52:56 +0800

    add

--- main.py
+++ main.py
@@ -34,19 +34,20 @@ import pylab as pl
 # sir_model_ba.main()
 total_links=300
 rounds=10
-times=np.zeros(int(total_links/10)+1)
+interval=10
+times=np.zeros(int(total_links/interval)+1)
 add_links=0
 x=[]
-while add_links<=total_links/10:
+while add_links<=total_links/interval:
     for i in range(rounds):
-        sir_model_ba=SIR(beta=0.02,miu=0.1,t=100,network="Add_Links_Random",method="max_node",add_links=add_links)
+        sir_model_ba=SIR(beta=0.02,miu=0.1,t=100,network="Add_Links_By_Betweenness",method="max_node",add_links=add_links)
         times[add_links]+=sir_model_ba.main()
     times[add_links]=times[add_links]/rounds
 
     x.append(add_links)
     add_links += 1
 x=np.array(x)
-plt.plot(x*10,times, '-k', label='Susceptibles')
+plt.plot(x*interval,times, '-k', label='Susceptibles')
 plt.legend(loc=0)
 plt.title('SIR_Model with links adding randomly')
 plt.xlabel('add_links')
